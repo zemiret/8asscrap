8167ebd1f58c171016ffe7a46cca55c4dc9ae2ae
zemiret <dev@amleczko.pl>
2024-06-05 22:41:56 +0200

    wip wip wip. trying to get request API calls to work, which is tricky...

--- data_collector/src/lib.rs
+++ data_collector/src/lib.rs
@@ -1,4 +1,6 @@
-use std::{collections::HashMap, process::Command};
+use std::{arch::x86_64::_mm_cmpord_pd, collections::HashMap, process::Command};
+
+use reqwest::header::{HeaderMap, USER_AGENT};
 
 pub struct Client {
     http_client: reqwest::Client,
@@ -37,35 +39,87 @@ impl Client {
     ) -> Result<HashMap<String, String>, Box<dyn std::error::Error>> {
         // We get 401 on sid expired or wrong user-agent
 
+        // Okaaaaay, this works... DAMN. IT KINDA LOOK LIKE IT MIGHT HAVE TO DO WITH SMALL/BIG HEADERS!
+        // So i'll need to use some different library as reqwest normalizes all requests to lowercase...
+        let mut tmpcmd = Command::new("curl");
+        let cmd = tmpcmd
+            .args([
+                "https://www.8a.nu/unificationAPI/ascent/v1/web/users/antoni-mleczko/ascents?category=sportclimbing&pageIndex=0&pageSize=1",
+                "-H",
+                "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:124.0) Gecko/20100101 Firefox/124.0",
+                "-H",
+                "Cookie: connect.sid=s%3AH519MDgMrFtbrlz4ZXr28kw481eeDRXk.tysjm69u%2BUkCxcDrUjIt56H0zItzk%2FN%2BZD%2FhRi3bZRc;"
+            ]);
+            // .args([
+            //     "https://www.8a.nu/unificationAPI/ascent/v1/web/users/antoni-mleczko/ascents?category=sportclimbing&pageIndex=0&pageSize=1",
+            //     "-H",
+            //     "user-ugent: Mozilla/5.0 (X11; Linux x86_64; rv:124.0) Gecko/20100101 Firefox/124.0",
+            //     "-H",
+            //     "cookie: connect.sid=s%3AH519MDgMrFtbrlz4ZXr28kw481eeDRXk.tysjm69u%2BUkCxcDrUjIt56H0zItzk%2FN%2BZD%2FhRi3bZRc;"
+            // ]);
+        println!("{:#?}", cmd);
+        let cmdres = cmd.output()?;
+
+        let cmd_out = String::from_utf8(cmdres.stdout).unwrap(); // TODO: Handle error nicer
+        if !cmdres.status.success() {
+            panic!("cmd fail: {}", cmd_out); // TODO: HANDLE ERROR NICELY
+        }
+        println!("{}", cmd_out);
+
 
         // TODO: HANDLE PAGING
         // TODO: HANDLE 401 (call authenticate and retry operation)
         // TODO: Bouldering category too
-        let resp = self
+
+        let mut headers = HeaderMap::new();
+
+        headers.insert("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:124.0) Gecko/20100101 Firefox/124.0".parse().unwrap());
+        headers.insert("Cookie", format!("connect.sid={};", self.connect_sid).parse().unwrap());
+
+        let req = self
             .http_client
             .get(format!(
-                "https://www.8a.nu/unificationAPI/ascent/v1/web/users/{}/ascents", user
+                "https://www.8a.nu/unificationAPI/ascent/v1/web/users/antoni-mleczko/ascents?category=sportclimbing&pageIndex=0&pageSize=1",
             ))
-            .query(&[
-                ("category", "sportclimbing"),
-                ("pageIndex", "0"),
-                ("pageSize", "1000"),
-                ("sortField", "date_desc"),
-                ("timeFilter", "0"),
-                ("gradeFilter", "0"),
-                ("typeFilter", ""),
-                ("includeProjects", "false"),
-                ("searchQuery", ""),
-                ("showRepeats", "false"),
-                ("showDuplicates", "false"),
-            ])
-            .header(
-                "User-Agent",
-                "Mozilla/5.0 (X11; Linux x86_64; rv:124.0) Gecko/20100101 Firefox/124.0",
-            )
-            .header("Cookie", format!("connect.sid={}", self.connect_sid))
-            .send()
-            .await?;
+            .headers(headers)
+            // .header(
+            //     USER_AGENT,
+            //     "Mozilla/5.0 (X11; Linux x86_64; rv:124.0) Gecko/20100101 Firefox/124.0",
+            // )
+            // .header("Cookie", format!("connect.sid={};", self.connect_sid)).build()?;
+            .build()?;
+
+        println!("REQUEST:");
+        println!("{req:#?}");
+
+        // let resp = req.send().await?;
+        let resp = self.http_client.execute(req).await?;
+
+        // let resp = self
+        //     .http_client
+        //     .get(format!(
+        //         "https://www.8a.nu/unificationAPI/ascent/v1/web/users/{}/ascents", user
+        //     ))
+        //     .query(&[
+        //         ("category", "sportclimbing"),
+        //         ("pageIndex", "0"),
+        //         ("pageSize", "1"),
+        //         // ("sortField", "date_desc"),
+        //         // ("timeFilter", "0"),
+        //         // ("gradeFilter", "0"),
+        //         // ("typeFilter", ""),
+        //         // ("includeProjects", "false"),
+        //         // ("searchQuery", ""),
+        //         // ("showRepeats", "false"),
+        //         // ("showDuplicates", "false"),
+        //     ])
+        //     .header(
+        //         "User-Agent",
+        //         "Mozilla/5.0 (X11; Linux x86_64; rv:124.0) Gecko/20100101 Firefox/124.0",
+        //     )
+        //     .header("Cookie", format!("connect.sid={};", self.connect_sid))
+        //     .send()
+        //     .await?;
             // .json::<HashMap<String, String>>()
             // .await?;
 
